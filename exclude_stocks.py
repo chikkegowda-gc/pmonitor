85b8b8741c9066f1daf52fff715fb26c4da7a3ff
chikkegowda chikkaiah <gcchikke@gmail.com>
2025-10-08 09:59:47 -0400

    exclude tickers

--- exclude_stocks.py
+++ exclude_stocks.py
@@ -1,2 +1,1 @@
-exclude_tickers:["ALFIQ", "NBIS", 'RDDT', 'TSLL', "INTW", "AAPL"]
-
+exclude_tickers = ["ALFIQ", "NBIS", 'RDDT', 'TSLL', "INTW", "RGTI", "MSTR", "SOXl", "SOXS"]  # Example tickers to exclude
